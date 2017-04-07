17601a5568fc507602e2ed9ef448feab4858f7ef
Ga1ahad <adam97n@gmail.com>
2017-04-07 21:29:10 +0200

    Add files via upload

---
+++ ex2/Calc.java
@@ -0,0 +1,46 @@
+/**
+ *
+ *  @author Nguyen Adam S15264
+ *
+ */
+
+package ex2;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Calc {
+	
+    public String doCalc(String arg) {    
+    	
+    	BigDecimal a;
+ 	    BigDecimal b;
+ 	    BigDecimal c;
+ 	    
+ 	    String arr[];
+ 	    
+ 	    MathContext mc = new MathContext(10);
+ 	    Map<String, Method> sign = new HashMap<String, Method>();
+    	    try {
+    	    	sign.put("-", BigDecimal.class.getMethod("subtract", BigDecimal.class));
+    	    	sign.put("+", BigDecimal.class.getMethod("add", BigDecimal.class));
+    	    	sign.put("*", BigDecimal.class.getMethod("multiply", BigDecimal.class));
+    	    	sign.put("/", BigDecimal.class.getMethod("divide", BigDecimal.class,MathContext.class));              
+    	    	arr = arg.split(" "); 
+
+                a = new BigDecimal(arr[0]);
+                b = new BigDecimal(arr[2]);
+                c = (BigDecimal) sign.get(arr[1]).invoke(a, b, mc);
+                
+                return String.valueOf(c);
+                
+            } catch (Exception e) {
+                return "Invalid command to calc";
+            }
+    	    
+    }
+ 
+
+}
---
+++ ex2/Main.java
@@ -0,0 +1,19 @@
+/**
+ *
+ *  @author Nguyen Adam S15264
+ *
+ */
+
+package ex2;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+    Calc c = new Calc();
+    String result = c.doCalc("args[0]");
+    System.out.println(result);
+  }
+  	
+}
+	
